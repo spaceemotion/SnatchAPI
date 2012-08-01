23300f8ecba0f98cea0ab1f41e5f0e39121896c1
SpaceEmotion <spaceemotion@me.com>
2012-08-01 13:55:14 +0200

    Added XML Helper

---
+++ system/helpers/XMLHelper.php
@@ -0,0 +1,31 @@
+<?php
+
+	/**
+	 * XML functions
+	 *
+	 * @version 1.0
+	 */
+	class XMLHelper {
+		/**
+		 * Converts an array into a SimpleXmlElement
+		 *
+		 * @return SimpleXmlElement
+		 */
+		public static function array2xml($arr, &$xml) {
+			foreach ($arr as $key => $value) {
+				if (is_array($value)) {
+		    		if (!is_numeric($key)) {
+						$sub = $xml->addChild("$key");
+						self::array2xml($value, $sub);
+					} else {
+						self::array2xml($value, $xml);
+					}
+				} else {
+					$xml->addChild("$key", "$value");
+				}
+			}
+		}
+	}
+	
+
+?>
